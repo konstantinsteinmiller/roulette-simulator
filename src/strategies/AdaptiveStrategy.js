b751225764059e23fbced040c1bfaa552548c39b
Konstantin Steinmiller <konstantin.steinmiller@factset.com>
2020-03-26 21:12:42 +0100

    add an adaptive strategy that uses 1/12 below 40 lossesInARow and 5/6 afterwards to win back fast.

--- src/strategies/AdaptiveStrategy.js
+++ src/strategies/AdaptiveStrategy.js
@@ -1,30 +1,52 @@
 export default {
-  name: 'safe return 1/12',
+  name: 'adaptive',
   minBetAmount: 1,
   betLooseMultiplier: 1,
   betWinMultiplier: 8,
   /* eslint-disable */
-  // selectedItems: [0, 1, 2, 3],
-  // selectedItems: [33, 34, 35, 36],
-  // selectedItems: [31, 33, 34, 30],
-  // selectedItems: [27, 28, 29, 0],
   runningSelectedItemsAmount: 4,
-  runningSelectedItemsOffset: 1,
+  runningSelectedItemsOffset: 0,
   /* eslint-enable */
-  onLoose(betAmount, minBetAmount, betQueue, statistics) {
+  onLoose(betAmount, minBetAmount, betQueue, statistics, that) {
     statistics.lostCashOnLossStreak += betAmount
     const newBetAmount = Math.ceil(
       statistics.lostCashOnLossStreak / this.betWinMultiplier
     )
-    const increasedBetAmount =
+    let increasedBetAmount =
       this.betWinMultiplier * betAmount < statistics.lostCashOnLossStreak
         ? newBetAmount
         : betAmount
 
+    if (statistics.lossesInARow > 20 && that.strategy.betWinMultiplier === 8) {
+      that.strategy.betWinMultiplier = 1
+      that.strategy.betLooseMultiplier = 5
+      that.strategy.runningSelectedItemsAmount = 30
+      that.betWinMultiplier = 1
+      that.betLooseMultiplier = 5
+      statistics.lostCashOnLossStreak +=
+        betAmount * that.strategy.betLooseMultiplier - 1
+      const adaptedBetAmount = Math.ceil(
+        (betAmount * that.strategy.betLooseMultiplier) / 2
+      )
+      increasedBetAmount =
+        that.strategy.betLooseMultiplier * betAmount <=
+        statistics.lostCashOnLossStreak
+          ? adaptedBetAmount
+          : betAmount
+
+      betQueue.unshift(JSON.parse(JSON.stringify(increasedBetAmount)))
+    }
+
     betQueue.unshift(JSON.parse(JSON.stringify(increasedBetAmount)))
     return { betAmount, minBetAmount, betQueue, statistics }
   },
-  onWin(/*betAmount, minBetAmount, betQueue*/) {
-    // console.log('onWin: ')
+  onWin(betAmount, minBetAmount, betQueue, statistics, that) {
+    if (statistics.lossesInARow === 0 && that.strategy.betWinMultiplier !== 8) {
+      that.strategy.betWinMultiplier = 8
+      that.strategy.betLooseMultiplier = 1
+      that.strategy.runningSelectedItemsAmount = 4
+      that.betWinMultiplier = 8
+      that.betLooseMultiplier = 1
+    }
   },
 }
